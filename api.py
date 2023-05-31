e374909b8dc1c76b1e2759f8043d119dcab0f590
raphaelian-dev <raphael.laperrousaz@gmail.com>
2023-05-31 19:20:26 +0200

    User sessions in api.py

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
 import sqlite3
 import hashlib
+from random import choices
+from string import ascii_letters, digits, punctuation
+characters = list(ascii_letters + digits + punctuation)
+characters.remove("'")
+from datetime import date
 
 class Database():
-    #
+    # Inits the python object. It has as a property a object which can be use to access the database located in the file of given name plus the .db extension. Creates if they do not exist the data tables for the products, the orders and the users.
     def __init__(self, db_name:str):
         self.types=('inhabitable-planet','not-inhabitable-planet','moon','star-in-system','star-without-system')
         self.db = sqlite3.connect('./data/'+db_name+'.db')
@@ -39,7 +44,9 @@ CREATE TABLE IF NOT EXISTS users
     name VARCHAR(50),
     firstName VARCHAR(50),
     permissionLevel INT DEFAULT 0,
-    certified INT DEFAULT 0
+    certified INT DEFAULT 0,
+    session VARCHAR(32),
+    sessiondate CHAR(7)
 )
 """)
         self.db.commit()
@@ -50,7 +57,7 @@ SELECT * FROM products ORDER BY price
 """)
         products = cursor.fetchall()
         return products
-    #
+    # Returns the HTML code usable for the products list in the website.
     def get_all_products_HTML(self) -> str:
         HTML_code = ''
         products = self.get_all_products()
@@ -60,7 +67,8 @@ SELECT * FROM products ORDER BY price
             openpopup = 'onclick="openpopup(\''+i[4]+'\',\''+i[5]+'\',\''+type_name+'\')"'
             HTML_code += '<div class="product '+type_name+'"><div class="properties-absolute"><div class="product-picture"><img src="/images/'+name+'.'+i[1]+'" onclick="openOrderPopup(\''+name+'\')"><div class="price">'+str(i[3])+' STR</div></div><h3 class="product-title">'+name+'</h3><a class="more-info" '+openpopup+'></a></div></div>'
         return HTML_code
-    #
+    # --- Operations on the products table of the database. ---
+    # Registers a new product to the database. Returns True if operation successful, else False.
     def add_product(self, name:str, imageExtension:str, product_type:int, price:int, description:str='We do not know much yet about this celestial body.', color:str='#808080') -> bool:
         if None in (name,product_type,price) or '' in (name,product_type,price) or product_type>=len(self.types):
             return False
@@ -72,6 +80,8 @@ INSERT INTO products (name, imageExtension, type, price, description, color)
 VALUES ("""+str([name,imageExtension,product_type,price,description,color])[1:-1].replace("\\\\'", "\\'")+""")
 """)
         self.db.commit()
+        return True
+    # Removes a registered product from the database.
     def remove_product(self, name:str):
         cursor = self.db.cursor()
         cursor.execute("""
@@ -79,8 +89,9 @@ DELETE FROM products WHERE name='"""+name+"""'
 """)
         self.db.commit()
         return True
-    #
-    def add_order(self, product_name:str, email:str, name:str=None, first_name:str=None) -> bool:
+    # --- Operations on the orders table of the database. ---
+    # Registers an order to the database. Return True if operation successful, else False.
+    def add_order(self, product_name:str, email:str, name:str|None=None, first_name:str|None=None) -> bool:
         cursor = self.db.cursor()
         cursor.execute("""
 SELECT * FROM products WHERE name='"""+product_name+"""'
@@ -108,7 +119,7 @@ VALUES ("""+str(values)[1:-1]+""")
         self.db.commit()
         self.remove_product(product_name)
         return True
-    #
+    # Cancels an order registered in the database.
     def cancel_order(self, product_name:str):
         cursor = self.db.cursor()
         cursor.execute("""
@@ -125,8 +136,9 @@ SELECT productName, imageExtension, type, price, description, color FROM orders 
 DELETE FROM orders WHERE productName='"""+product_name+"""'
 """)
         self.db.commit()
-    #
-    def add_user(self, email:str, password:str, name:str=None, first_name:str=None) -> int:
+    # --- Operations on the users table of the database. ---
+    # Registers a user to the database. Returns -1 if no email or password given, 1 if user already exists or 0 if operation successful.
+    def add_user(self, email:str, password:str, name:str|None=None, first_name:str|None=None) -> int:
         cursor = self.db.cursor()
         if None in (email, password) or '' in (email, password):
             return -1
@@ -148,7 +160,51 @@ VALUES ("""+str(values)[1:-1]+""")
 """)
         self.db.commit()
         return 0
+    # Returns True if given password is correct for the user with given email, else False.
+    def check_password(self, email:str, password:str) -> bool:
+        hashed_password = hash_password(password)
+        cursor = self.db.cursor()
+        cursor.execute("SELECT hashedPassword FROM users WHERE email='"+email+"'")
+        real_hashed_tuple = cursor.fetchone()
+        if real_hashed_tuple == None:
+            return False
+        return hashed_password == real_hashed_tuple[0]
+    # Registers a user session identifiable by its 32 characters random ascii string. Returns None in case of wrong password else returns the generated id.
+    def create_session(self, email:str, password:str) -> str|None:
+        if self.check_password(email, password):
+            cursor = self.db.cursor()
+            session = random_asciistr_32()
+            print(session)
+            while not cursor.execute("SELECT session FROM users WHERE session='"+session+"'"):
+                session = random_asciistr_32()
+            cursor.execute("UPDATE users SET session='"+session+"',sessiondate='"+datestr()+"' WHERE email='"+email+"'")
+            self.db.commit()
+            return session
+        else:
+            return None
+    # Deletes a user session.
+    def delete_session(self, session:str):
+        cursor = self.db.cursor()
+        cursor.execute("UPDATE users SET session=NULL, sessiondate=NULL WHERE session='"+session+"'")
+        self.db.commit()
+    # Checks if user session is valid. Returns session user's email if so, else None.
+    def check_session(self, session:str) -> str|None:
+        cursor = self.db.cursor()
+        session_attributes = cursor.execute("SELECT email, sessiondate FROM users WHERE session='"+session+"'").fetchone()
+        if session_attributes == None:
+            return None
+        email, sessiondate = session_attributes
+        if sessiondate != datestr():
+            self.delete_session(session)
+            return None
+        return email
 
 
-def hash_password(pswd):
+def hash_password(pswd:str) -> str:
     return hashlib.sha256(pswd.encode()).hexdigest()
+
+def random_asciistr_32() -> str:
+    return "".join(choices(characters, k=32))
+
+def datestr() -> str:
+    return str(date.today()).replace('-','')
